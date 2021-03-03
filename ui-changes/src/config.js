4b3f937facfd7ec2016141b7fc4c72e180455413
Frances <40801017+MulliganF02@users.noreply.github.com>
2021-03-03 11:11:13 -0500

    ui fixes

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -3,6 +3,6 @@ module.exports = {
   githubSrc: 'https://raw.githubusercontent.com/MulliganF02/InfantImaging/master/InfantImaging/InfantImaging_schema',
   banner: 'This is a demonstration protocol for ReproSchema.',
   startButton: 'Join',
-  assetsPublicPath: '/InfantImaging/',
+  assetsPublicPath: '/infantimaging/',
   backendServer: null
 };
