9a0c70036a978e1c3072d2a13e67ad4a82a6a7a6
Frances <40801017+MulliganF02@users.noreply.github.com>
2021-03-03 10:58:10 -0500

    testing fixes

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/ReproNim/demo-protocol/master/DemoProtocol/DemoProtocol_schema',
+  githubSrc: 'https://raw.githubusercontent.com/MulliganF02/InfantImaging/master/InfantImaging/InfantImaging_schema',
   banner: 'This is a demonstration protocol for ReproSchema.',
   startButton: 'Join',
-  assetsPublicPath: '/demo-protocol/',
+  assetsPublicPath: '/InfantImaging/',
   backendServer: null
 };
